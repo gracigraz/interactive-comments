3c131befddf283085a844bdf3deac8eb3cde7c09
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2024-03-13 15:15:06 -0400

    added main page

---
+++ src/pages/CommentsPage/CommentsPage.tsx
@@ -0,0 +1,12 @@
+import "./CommentsPage.scss";
+
+
+// This page serves as the main interface for the interactive comments app
+const CommentsPage = ()=> {
+    return (
+        <>
+        </>
+        )
+}
+
+export default CommentsPage;
