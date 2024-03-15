46a23970ae76bbf9edb889c39bd6a990bc6048c3
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2024-03-15 13:17:23 -0400

    added replies data and styling

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -7,10 +7,52 @@ import replyIcon from '../../assets/images/icons/icon-reply.svg';
 
 import commentsData from '../../data.json';
 
-const Comment = () =>{
+interface User {
+    username: string;
+    image: { png: string; webp: string };
+  }
+  
+  interface Reply {
+    id: number;
+    content: string;
+    createdAt: string;
+    score: number;
+    user: User;
+  }
+  
+  interface CommentProps {
+    id: number;
+    content: string;
+    createdAt: string;
+    score: number;
+    user: User;
+    replies?: Reply[];
+  }
+  // Assume data has the correct structure.
+const data = commentsData as { comments: CommentProps[] };
+
+const Comment: React.FC = () =>{
+    const renderReplies = (replies: Reply[]) => {
+        return replies.map((reply) => (
+            <div key={reply.id} className="comments__reply">
+                {/* Reply structure similar to comment */}
+                <div className="comments__top-div">
+                    <img className="comments__profile" src={reply.user.image.png} alt={reply.user.username}/>
+                    <span className="comments__username">{reply.user.username}</span>
+                    <span className="comments__date">{reply.createdAt}</span>
+                </div>
+                <p className="comments__body">{reply.content}</p>
+                <div className="comments__bottom-div">
+                    <LikeButton score={reply.score}/>
+                    <Button textBtn="Reply" imgSrc={replyIcon} imgAlt="Reply icon" textColor="#4448C5"/>  
+                </div>
+            </div>
+        ));
+    };
     return(
         <div className="comments">
                {commentsData.comments.map((comment) => (
+                <>
                  <div key={comment.id} className="comments__container">
             <div className="comments__top-div">
                 <img className="comments__profile" src={comment.user.image.png} alt={comment.user.username}/>
@@ -24,7 +66,13 @@ const Comment = () =>{
                 {/* <Button textBtn="Delete" imgSrc={deleteIcon} imgAlt="Delete icon" textColor="#ec6368"/>  
                 <Button textBtn="Edit" imgSrc={editIcon} imgAlt="Edit icon" textColor='#4448C5'/>   */}
                 </div>
+               
             </div>
+            <div  className="comments__reply-container">
+                 {/* Render replies if any */}
+                 {comment.replies && renderReplies(comment.replies)}
+            </div>
+            </>
             ))}
         </div>
     )
